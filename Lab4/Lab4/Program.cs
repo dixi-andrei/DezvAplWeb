f4d98430ccca529e77517ae1849adbb52a916f1c
dixi-andrei <andrei-adelin.ditoiu@s.unibuc.ro>
2023-11-24 20:36:51 +0200

    tema7

--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<lab4context>(options =>
 {
-    options.UseSqlServer("Server=DESKTOP-OPDT9A3\\SQLEXPRESS;Database=laborato4;Trusted_Connection=True;TrustServerCertificate=True;");
+    options.UseSqlServer("Server=DESKTOP-OPDT9A3\\SQLEXPRESS;Database=laboratu4;Trusted_Connection=True;TrustServerCertificate=True;");
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
